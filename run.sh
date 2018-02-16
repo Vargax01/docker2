8f255b36e7f2bf72ea0cc453d6177b75314481e0
Vagrant Default User <vagrant@ftp>
2018-02-16 09:02:39 +0000

    para docker hub

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Esto para el ejercicio 2 de iaw
 sed -i 's/root/'$MYSQL_ENV_MYSQL_USER'/g' /var/www/html/core/controller/Database.php
 sed -i 's/pass=""/pass="'$MYSQL_ENV_MYSQL_PASSWORD'"/g' /var/www/html/core/controller/Database.php
 sed -i 's/localhost/'$MYSQL_PORT_3306_TCP_ADDR'/g' /var/www/html/core/controller/Database.php
